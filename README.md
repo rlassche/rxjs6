6d25f44081c6339e3a3cf66d06412d9d3b471ff9
Your Name <you@example.com>
2018-09-01 18:54:59 +0200

    .

--- README.md
+++ README.md
@@ -5,3 +5,40 @@ Here are some coding examples the Angular HttpClient and REST calls with GET, PO
 
 JSON placeholder is used for the backend.
 
+# GET
+
+```
+this.http.get<any>(this.jsonPH + uri)
+      .subscribe(data => {
+        this.result = data;
+      },
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            this.errorMessage = 'Client side Error occured';
+          } else {
+            this.errorMessage = 'Server side Error occured';
+          }
+        });
+  }
+```
+
+# POST
+
+```
+ this.toPost = { title: 'foo', body: 'bar', userId: 1 };
+ his.http.post<any>(this.jsonPH + uri, this.toPost)
+      .subscribe(
+        res => { console.log(res); this.result = res },
+        (err: HttpErrorResponse) => { console.log(err); this.errorMessage = err.message }
+      )
+```
+
+# PUT
+```
+this.toPut = { id: 1, title: 'foo', body: 'bar', userId: 1 };
+this.http.put<any>(this.jsonPH + uri, this.toPut)
+      .subscribe(
+        (res: any) => { console.log(res); this.result = res },
+        (err: HttpErrorResponse) => { console.log(err); this.errorMessage = err.message }
+      )
+```
--- src/app/test-rest/test-rest.component.ts
+++ src/app/test-rest/test-rest.component.ts
@@ -1,60 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpResponse,   HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpResponse, HttpParams } from '@angular/common/http';
 @Component({
   selector: 'app-test-rest',
   templateUrl: './test-rest.component.html',
   styleUrls: ['./test-rest.component.css']
 })
 export class TestRESTComponent implements OnInit {
-  jsonPH:string  = "https://jsonplaceholder.typicode.com";
+  jsonPH: string = "https://jsonplaceholder.typicode.com";
   result;
   command;
   errorMessage;
   toPost;
   toPut;
-  constructor( private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   getTest(uri) {
-    this.errorMessage=''
+    this.errorMessage = ''
     this.command = uri;
-    this.result='' 
+    this.result = ''
     this.http.get<any>(this.jsonPH + uri)
-      .subscribe( data=> {
-          this.result = data;
+      .subscribe(data => {
+        this.result = data;
       },
-    (err: HttpErrorResponse) => {
-        if( err.error instanceof Error ){
-          this.errorMessage = 'Client side Error occured';
-        } else {
-          this.errorMessage = 'Server side Error occured';
-        }
-    }); 
+        (err: HttpErrorResponse) => {
+          if (err.error instanceof Error) {
+            this.errorMessage = 'Client side Error occured';
+          } else {
+            this.errorMessage = 'Server side Error occured';
+          }
+        });
   }
 
   postTest(uri) {
-    this.errorMessage=''
-    this.result=''
-    this.toPost = { title:'foo', body:'bar', userId: 1};
-    this.command = '/post   ' + JSON.stringify( this.toPost)
+    this.errorMessage = ''
+    this.result = ''
+    this.toPost = { title: 'foo', body: 'bar', userId: 1 };
+    this.command = '/post   ' + JSON.stringify(this.toPost)
     this.http.post<any>(this.jsonPH + uri, this.toPost)
-        .subscribe(
-          res => { console.log( res ); this.result = res },
-          (err:HttpErrorResponse) => { console.log( err ); this.errorMessage = err.message}
-        )
+      .subscribe(
+        res => { console.log(res); this.result = res },
+        (err: HttpErrorResponse) => { console.log(err); this.errorMessage = err.message }
+      )
   }
 
   putTest(uri) {
-    this.errorMessage=''
-    this.result=''
-    this.toPut = { id:1, title:'foo', body: 'bar', userId: 1};
-    this.command = uri + '  ' + JSON.stringify( this.toPut)
+    this.errorMessage = ''
+    this.result = ''
+    this.toPut = { id: 1, title: 'foo', body: 'bar', userId: 1 };
+    this.command = uri + '  ' + JSON.stringify(this.toPut)
     this.http.put<any>(this.jsonPH + uri, this.toPut)
-        .subscribe(
-          (res:any) => { console.log( res ); this.result = res },
-          (err:HttpErrorResponse) => { console.log( err ); this.errorMessage = err.message}
-        )
+      .subscribe(
+        (res: any) => { console.log(res); this.result = res },
+        (err: HttpErrorResponse) => { console.log(err); this.errorMessage = err.message }
+      )
   }
 }

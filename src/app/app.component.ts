410b37d78e45a56b04b40b09620840040e0deb97
Your Name <you@example.com>
2018-09-01 19:40:19 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
-
+import { WebsocketService } from './websocket.service';
+import { ChatService } from './chat.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [WebsocketService, ChatService]
 })
 export class AppComponent {
   title = 'rxjs6';
+  constructor(private chatService: ChatService) {
+    chatService.messages.subscribe(msg => {
+      console.log("Response from server: " , msg);
+    });
+  }
+  private message = {
+		author: 'tutorialedge',
+		message: 'this is a test message'
+  }
+  
+  sendMsg() {
+		console.log('sendMsg: message from client to server: ', this.message);
+		this.chatService.messages.next(this.message);
+		this.message.message = '';
+	}
 }

3def45e758ecdc0647966eebc18073e48708b25e
Your Name <you@example.com>
2018-09-02 11:34:24 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,31 @@ import { ChatService } from './chat.service';
 export class AppComponent {
   title = 'rxjs6';
   constructor(private chatService: ChatService) {
-    chatService.messages.subscribe(msg => {
-      console.log("Response from server: " , msg);
-    });
+    chatService.messages.subscribe(
+      (msg:MessageEvent) => {
+        //console.log("Response from server: ", msg.data);
+        if( msg.type == 'message') {
+          console.log("Response from server: ", msg.data);
+        } else {
+          console.log( "Response: ", msg )
+        }
+      },
+      (err:Event) => {
+        if( err.type == 'error') {
+          console.log("ERROR with url: ", err.currentTarget['url'] )
+        } else {
+          console.log("ERROR : ", err)
+        }
+      });
   }
   private message = {
-		author: 'tutorialedge',
-		message: 'this is a test message'
+    author: 'tutorialedge',
+    message: 'this is a test message from the client'
   }
-  
+
   sendMsg() {
-		console.log('sendMsg: message from client to server: ', this.message);
-		this.chatService.messages.next(this.message);
-		this.message.message = '';
-	}
+    console.log('sendMsg: message from client to server: ', this.message);
+    this.chatService.messages.next(this.message);
+    this.message.message = '';
+  }
 }
--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -1,38 +1,51 @@
 import { Injectable } from '@angular/core';
-import * as Rx from 'rxjs';
+//import * as Rx from 'rxjs';
+import { Observable, Observer, Subject } from 'rxjs';
 
+export interface Message {
+  author: string,
+  message: string
+}
 @Injectable()
 export class WebsocketService {
   constructor() { }
 
-  private subject: Rx.Subject<MessageEvent>;
+  private subject: Subject<MessageEvent>;
 
-  public connect(url): Rx.Subject<MessageEvent> {
+  public connect(url): Subject<MessageEvent> {
     if (!this.subject) {
       this.subject = this.create(url);
-      console.log("Successfully connected: " + url);
+      console.log("Successfully connected to: " + url);
     }
     return this.subject;
   }
 
-  private create(url): Rx.Subject<MessageEvent> {
-    let ws = new WebSocket(url);
+  private create(url): Subject<MessageEvent> {
+    let ws: WebSocket = new WebSocket(url);
 
-    let observable = Rx.Observable.create(
-      (obs: Rx.Observer<MessageEvent>) => {
-        ws.onmessage = obs.next.bind(obs);
-        ws.onerror = obs.error.bind(obs);
-        ws.onclose = obs.complete.bind(obs);
-        return ws.close.bind(ws);
-      })
+    //
+    //      OBSERVABLE
+    //
+    let observable = Observable.create((obs: Observer<MessageEvent>) => {
+      console.log('Observable.create');
+      ws.onmessage = obs.next.bind(obs);
+      ws.onerror = obs.error.bind(obs);
+      ws.onclose = obs.complete.bind(obs);
+      return ws.close.bind(ws);
+    })
+
+    //
+    //      OBSERVER
+    //
     let observer = {
-      next: (data: Object) => {
+      next: (data: Message) => {
         if (ws.readyState === WebSocket.OPEN) {
+          console.log('observer: data: ', data);
           ws.send(JSON.stringify(data));
         }
       }
     }
-    return Rx.Subject.create(observer, observable);
+    return Subject.create(observer, observable);
   }
 
 }
